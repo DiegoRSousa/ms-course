2f516216f40fedf9277e0a0027d240e345b8929a
Diego Sousa <diegors90@gmail.com>
2021-02-17 21:15:20 -0300

    Add RestTemplate

---
+++ hr-payroll/src/main/java/com/diego/hrpayroll/model/Worker.java
@@ -0,0 +1,62 @@
+package com.diego.hrpayroll.model;
+
+import java.math.BigDecimal;
+
+public class Worker {
+
+	private Long id;
+	private String name;
+	private BigDecimal dailyIncome;
+	
+	public Worker() {}
+	public Worker(Long id, String name, BigDecimal dailyIncome) {
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(BigDecimal dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
--- hr-payroll/src/main/java/com/diego/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/diego/hrpayroll/service/PaymentService.java
@@ -1,15 +1,31 @@
 package com.diego.hrpayroll.service;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.diego.hrpayroll.model.Payment;
+import com.diego.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
-
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerRost;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", new BigDecimal("200.00"), days);
+		var uriVariables = new HashMap<String, String>();
+		uriVariables.put("id", ""+workerId);
+		
+		var worker = restTemplate.getForObject(workerRost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
