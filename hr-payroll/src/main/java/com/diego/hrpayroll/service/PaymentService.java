701ba2fdf290bd562f24e858eba831fd82bb125c
Diego Sousa <diegors90@gmail.com>
2021-02-20 22:22:45 -0300

    Add Ribbon

--- hr-payroll/src/main/java/com/diego/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/diego/hrpayroll/service/PaymentService.java
@@ -1,20 +1,13 @@
 package com.diego.hrpayroll.service;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.diego.hrpayroll.feignClient.WorkerFeignClient;
 import com.diego.hrpayroll.model.Payment;
-import com.diego.hrpayroll.model.Worker;
 
 @Service
 public class PaymentService {
-	
-	@Autowired
-	private RestTemplate restTemplate;
 	
 	@Autowired
 	private WorkerFeignClient workerFeignClient;
