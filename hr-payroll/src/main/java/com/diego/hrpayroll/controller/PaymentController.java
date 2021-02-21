b3c2ae18601c6da6f6dc9a823219c7bac0f59a20
Diego Sousa <diegors90@gmail.com>
2021-02-20 23:41:29 -0300

    Add Hystrix

--- hr-payroll/src/main/java/com/diego/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/diego/hrpayroll/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.diego.hrpayroll.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.diego.hrpayroll.model.Payment;
 import com.diego.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("payments")
@@ -16,10 +19,17 @@ public class PaymentController {
 	
 	@Autowired
 	private PaymentService service;
-	
+
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable int days) {
 		var payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, @PathVariable int days) {
+		var payment = new Payment("Brann", new BigDecimal("400"), days);
+		return ResponseEntity.ok(payment);
+	}
+	
 }
