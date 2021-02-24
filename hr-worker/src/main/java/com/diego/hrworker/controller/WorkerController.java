9baf54d8bae6af1f63faf28e389951bd111936bc
Diego Sousa <diegors90@gmail.com>
2021-02-23 22:29:05 -0300

    Add Config Server

--- hr-worker/src/main/java/com/diego/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/diego/hrworker/controller/WorkerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.diego.hrworker.model.Worker;
 import com.diego.hrworker.repository.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping("workers")
 public class WorkerController {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
