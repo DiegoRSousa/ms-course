c970e47be62161779d00ee39137ae424fc9d9a57
Diego Sousa <diegors90@gmail.com>
2021-03-07 09:57:49 -0300

    add postrgres

--- hr-user/src/main/java/com/diego/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/diego/hruser/HrUserApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableTransactionManagement
 public class HrUserApplication implements CommandLineRunner{
 
 	@Autowired
--- hr-worker/src/main/resources/db/migration/V2__Initial.sql
+++ hr-worker/src/main/resources/db/migration/V2__Initial.sql
@@ -1,3 +1,4 @@
 INSERT INTO workers (name, daily_income) VALUES ('Bob', 200.00);
 INSERT INTO workers (name, daily_income) VALUES ('Maria', 300.00);
 INSERT INTO workers (name, daily_income) VALUES ('Alex', 250.00);
+INSERT INTO workers (name, daily_income) VALUES ('Pedro', 270.00);
